d5bc55d6f26e2b16a4a369568b3d825c348c0b50
a55678891 <a58176197@gmail.com>
2024-03-27 09:42:00 +0800

    problem1

--- hw1/src/NFinTech/NFinTech.sol
+++ hw1/src/NFinTech/NFinTech.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-import "@openzeppelin/contracts/utils/Address.sol";
 
 interface IERC721 {
     function balanceOf(address owner) external view returns (uint256 balance);
